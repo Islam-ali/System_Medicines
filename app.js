3a5f8ae83dc80c6ac0ee98403e3753b40b5bff79
islam-ali <you@example.com>
2024-02-19 02:45:41 +0200

    merge

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 var cors = require("cors");
 const { env } = require("process");
 var app = express();
+import serverless from "serverless-http";
 
 
 
@@ -62,6 +63,7 @@ app.use("/profit", require("./modules/profit/routes/profit.route"));
 // test afaq
 app.use("/map" , require("./modules/map/routes/map.route"))
 
+export const handler = serverless(app);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));

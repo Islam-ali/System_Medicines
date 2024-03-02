505d47704f0b16152708a6a1a8df92e7001bfe49
islam ali <ai4625511@gmail.com>
2024-03-02 21:35:35 +0200

    cros

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ var app = express();
 // import serverless from "serverless-http";
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
